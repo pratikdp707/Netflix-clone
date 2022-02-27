5ed14e1d9fd0a19c17d4aae1a27af7ca0014e26b
pratikdp707 <pratikparate707@gmail.com>
2022-02-27 10:00:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express  = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users')
+const movieRoute = require('./routes/movies')
+const listRoute = require('./routes/lists')
+const cors = require('cors')
+
+dotenv.config();
+app.use(cors());
+//Connecting with database
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(() => {
+    console.log('DB Connection Successfull');
+}).catch((error) => {
+    console.log(error);
+})
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/movie', movieRoute)
+app.use('/api/list', listRoute);
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000"); 
+})
